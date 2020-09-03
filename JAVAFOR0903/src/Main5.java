80919ae8782e8e9d42b44e7cbc3362a3ea3cee98
younwook <44747739+younwook@users.noreply.github.com>
2020-09-03 22:44:45 +0900

    Update Main5.java
    5000이넘어가는순간의 수를 구하는 예시

--- JAVAFOR0903/src/Main5.java
+++ JAVAFOR0903/src/Main5.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Main5 {
 
 	public static void main(String[] args) {
-		int sum =0;
-		Scanner sc = new Scanner(System.in);
-		int n= sc.nextInt();
-		for(int i=1;i<=n; i++) {
-			
-			sum += i;
-			
-		}
-		System.out.println("1ºÎÅÍ "+n+"±îÁöÀÇ ÇÕ°è´Â :"+sum);
+	int sum = 0;
+	int n = 0;
+		while (true) {
+			++n;
+			sum += n;    
+			if (sum > 5000) {
+			  break;
+			}
+		}      
+		System.out.println("1 ~ " + n + "ê¹Œì§€ì˜ í•©ê³„ : " + sum); 
 
 	}
 
